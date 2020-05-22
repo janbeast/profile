5426f1c13f2e031630fd7056ac0282931efc5d38
janbeast <65708414+janbeast@users.noreply.github.com>
2020-05-22 23:37:09 +0700

    Add files via upload

---
+++ profile/src/test/java/com/workshop/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
